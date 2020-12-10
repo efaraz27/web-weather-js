09906c20e9527604b010bd45cf7224f94898a5f2
Ehtesham Faraz <efaraz27@gmail.com>
2020-12-11 05:14:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+const api = {
+  key: "69f9c935d0191eb1c2a0fcf44d03d2d6",
+  base: "http://api.weatherstack.com/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults (query) {
+  fetch(`${api.base}current?access_key=${api.key}&query=${query}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.location.name}, ${weather.location.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.current.temperature)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.current.weather_descriptions[0];
+
+  let weather_time = document.querySelector('.current .time');
+  weather_time.innerText = weather.current.observation_time;
+  //let hilow = document.querySelector('.hi-low');
+  //hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
+
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
